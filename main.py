009c0e8163805ef364f6035e3726a510c9c79f20
chicken10dys <chicken@10dys.ca>
2023-04-11 14:01:27 -0400

    Overall improvement

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-import requests
 import os
 import sys
 import socket
 
-verson = input("What minecraft version will the server be?\n")
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+os.system("pip install requests")
+import requests
+
+verson = input("What minecraft version will the server be?\n")
 response = requests.get("https://api.papermc.io/v2/projects/paper/versions/" + verson)
 
 #Download the selected minecraft version
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 def manualOs():
     manualOs = input("Does your computer use .sh(linux/unix/mac) or .bat(windows) scripts?\n[.sh/.bat] ")
@@ -47,15 +65,15 @@ def setup():
     print("Now we will start setting up the server!")
     if sys.platform.startswith('win'):
         script = "windows"
-        if not input("We detected you're running windows so we will make a batch script. Is this correct?\n[y/n] ") == "y":
+        if not input("We detected you're running windows so we will make a batch script. Is this correct?\n[Y/N] ").lower() == "y":
             manualOs()
     elif sys.platform.startswith('linux'):
         script = "bash"
-        if not input("We detected you're running linux so we will make a bash script. Is this correct?\n[y/n] ") == "y":
+        if not input("We detected you're running linux so we will make a bash script. Is this correct?\n[Y/N] ").lower() == "y":
             manualOs()
     elif sys.platform.startswith('darwin'):
         script = "bash"
-        if not input("We detected you're running OSX so we will make a bash script. Is this correct?\n[y/n] ") == "y":
+        if not input("We detected you're running OSX so we will make a bash script. Is this correct?\n[Y/N] ").lower() == "y":
             manualOs()
     else:
         manualOs()
@@ -67,14 +85,27 @@ def setup():
         else:
             print("To continue you have to accept the EULA by pressing y")
 
-    ram = input("How much ram do you want to give to the server? (ex. 2G = 2 Gigabytes and 2M = 2 Megabytes\n")
+    ram = input("How much ram do you want to give to the server in Gigabytes? (ex. 2 = 2 Gigabytes)\n")
     if script == "bash":
         with open("run.sh", "w") as f:
-            f.write("#!/bin/bash\njava -Xmx" + ram + " -Xms" + ram + " -jar server.jar\necho \"Server off. Press enter to close\" \nread")
+            f.write("#!/bin/bash\njava -Xmx" + ram + "G -Xms" + ram + "G -jar server.jar\necho \"Server off. Press enter to close\" \nread")
         os.chmod("run.sh", 0o755)
     if script == "windows":
         with open("run.bat", "w") as f:
-            f.write("java -Xmx" + ram + " -Xms" + ram + " -jar server.jar\npause")
+            f.write("java -Xmx" + ram + "G -Xms" + ram + "G -jar server.jar\npause")
+        print("\nWhen the server opens for the first time you might see a windows firewall popup. If you do then allow it for both public and private networks.\n")
+
+    print("\nPort forwarding:\nFor other people to join your minecraft server you need to forward port 25565 to " + get_ip() + ".")
+    print("To do this you can try these links to access your router\'s settings:\n10.0.0.1\n192.168.0.1\n192.168.2.1")
+    if input("\n\nDo you want to test the server now?\n[Y/N]").lower() == "y":
+        if script == "bash":
+            os.system("./run.sh")
+        if script == "windows":
+            os.system("./run.bat")
+    if input("Did the server start?\n[Y/N] ").lower() == "n":
+        if input("Did it say something about java or JDK?\n[Y/N] ").lower() == "y":
+            print("Download the latest version of java from https://www.oracle.com/ca-en/java/technologies/downloads/ then run run.bat or run.sh")
+
 
 
 #Find the latest build of the selected minecraft version
